d030cd2599cc131f2db6d06392f657b1fedfe1cc
Miqueias97 <miqueiasnascimentonp@hotmail.com>
2024-06-06 23:09:58 -0300

    nova estruturação do código para reduzir redundancias e melhorar a legibilidade

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
+import requests, json
 import pandas as pd
 from datetime import datetime
-import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
-st.set_option('deprecation.showPyplotGlobalUse', False)
-st.set_page_config(layout="wide")
 
-df = pd.read_excel(r'hubspot-custom-report-devolucoes-2024-06-05.xls')
+url = 'https://script.google.com/macros/s/AKfycbwOFDUXCrRtMEQY4GXJ_jwp9x9Lp7NF3n06s9uU0NVF268iQo0jGvENvPBWHvWTalu4/exec'
 
-# Filtra Dataframe removendo No value
+list_data = []
+with requests.Session() as session:
+    for i, data in enumerate(requests.get(url).json()['status']):
+        if i == 0:
+            colunas = data
+        else:
+            list_data.append(data)
+
+df = pd.DataFrame.from_records(list_data, columns=colunas)
+
+classes_a_exibir = ['Troca', 'Abandono total', 'Abandono parcial', 'Upgrade', 'Lost piloto', 'Piloto parcial']
+
 filtros = (df['Quantidade Devolução - Calculado - Total'] != '(No value)') & (df['Ticket status'].isin(['Concluido', 'Devolução Cancelada'])) &\
-      (df['CO - Quantidade - Calculado - Total de dispositivos para devolução'] != '(No value)') & \
-        (df['Pedido data - Daily'] != '(No value)')
+    (df['CO - Quantidade - Calculado - Total de dispositivos para devolução'] != '(No value)') & \
+        (df['Pedido data - Daily'] != '(No value)') & (df['supply__devolucao__tipo'].isin(classes_a_exibir))
 
 df = df[filtros]
+
 lista = df.values.tolist()
 
 dic_lista = {}
@@ -23,10 +31,8 @@ for i in lista:
     qtd_prevista = int(i[6])
     qtd_feita = int(i[7])
     classe = i[9]
-    
-
     try:
-        data_realizacao = datetime.strptime(str(i[5]).split()[0], "%Y-%m-%d")
+        data_realizacao = datetime.strptime(str(i[5]).split('T')[0], "%Y-%m-%d")
         if dealId not in dic_lista:
             dic_lista[dealId] = {
                 'data_de_solicitacao' : data_de_solicitacao,
@@ -44,74 +50,44 @@ for i in lista:
     except:
         pass
 
-
-colunas = ['Deal Id', 'data_de_solicitacao', 'qtd_prevista', 'qtd_realizada', 'data_conclusão', 'classe', 'semana_solicitacao', 'semana_conclusao', 'semanas_em_aberto']
 list_df = []
-dia_da_semana = {
-    0 : 'Segunda',
-    1 : 'Terça',
-    2 : 'Quarta',
-    3 : 'Quinta',
-    4 : 'Sexta',
-    5 : 'Sábado',
-    6 : 'Domingo'
-}
 for i in dic_lista:
+    data_de_solicitacao = datetime.strptime(str(dic_lista[i]['data_de_solicitacao']).split('T')[0], '%Y-%m-%d')
+    semana_solicitacao = int(data_de_solicitacao.strftime("%U")) + 1
+    data_conclusao = dic_lista[i]['data_conclusão']
+    semana_conclusao = int(data_conclusao.strftime("%U")) + 1
+    semanas_em_aberto = abs(int(semana_conclusao) - int(semana_solicitacao))
+
+    # verifica Status
     if dic_lista[i]['qtd_prevista'] == dic_lista[i]['qtd_realizada']:
-        # Verifica dia da semana de solicitação
-        data_de_solicitacao = datetime.strptime(str(dic_lista[i]['data_de_solicitacao']).split()[0], '%Y-%m-%d')
-        semana_solicitacao = int(data_de_solicitacao.strftime("%U")) + 1
-        data_conclusao = dic_lista[i]['data_conclusão']
-        semana_conclusao = int(data_conclusao.strftime("%U")) + 1
-        semanas_em_aberto = abs(int(semana_conclusao) - int(semana_solicitacao))
+        status = 'Cocluído'
+    else:
+        status = 'Pendente'
         
+    
+    list_df.append( [i, dic_lista[i]['data_de_solicitacao'], dic_lista[i]['qtd_prevista'], dic_lista[i]['qtd_realizada'],\
+                    dic_lista[i]['data_conclusão'], dic_lista[i]['classe'], semana_solicitacao, semana_conclusao, semanas_em_aberto, status ] )
+    
 
-        list_df.append( [i, dic_lista[i]['data_de_solicitacao'], dic_lista[i]['qtd_prevista'], dic_lista[i]['qtd_realizada'],\
-                          dic_lista[i]['data_conclusão'], dic_lista[i]['classe'], semana_solicitacao, semana_conclusao, semanas_em_aberto ] )
-
+colunas = ['Deal Id', 'data_de_solicitacao', 'qtd_prevista', 'qtd_realizada', 'data_conclusão', 'classe', 'semana_solicitacao', 'semana_conclusao', 'semanas_em_aberto', 'status']
 
 df = pd.DataFrame.from_records(list_df, columns=colunas)
-df.to_csv('df.csv')
-# Filtra DF
 
-classes = df['classe'].unique().tolist()
-filtro_classe =st.sidebar.selectbox(
-    "Selecione a classe:",
-    classes)
+# inicia contrução do coHort
+maior_valor = (df['semana_solicitacao'].values.max() if df['semana_solicitacao'].values.max() >= df['semana_conclusao'].values.max() \
+               else df['semana_conclusao'].values.max()) + 1
 
-filtros = (df['classe'] == filtro_classe)
-df = df[filtros]
+estrutura_coHort = {}
+for cont, y in enumerate(range(1, maior_valor)):
 
-filtrar_por_semana = st.sidebar.checkbox("Deseja filtrar por semana?")
+    total_de_deals, total_de_dispositivos = 0 , 0
 
-if filtrar_por_semana:    
-    semanas_em_aberto = df['semana_solicitacao'].sort_values(ascending=True).unique().tolist()
-    semanas = st.sidebar.select_slider(
-        "Select a color of the rainbow",
-        options=semanas_em_aberto)
+    for x in range(0, maior_valor - cont):
+        filtro_df = df[(df.semana_solicitacao == y) & (df.semanas_em_aberto == x)]
+        dataFiltro = df[filtro_df]
+        print(dataFiltro)
+        
 
-    filtro = (df['semana_solicitacao'] <= semanas)
-    df = df[filtro]
-
-
-# Cria Tabela Dinamica
-tabela_dinamica = df.pivot_table(values='Deal Id', index='semana_solicitacao', columns='semanas_em_aberto', aggfunc='count') 
-tabela_dinamica.to_csv('dinamica.csv')
-
-# Grafico
-f, ax = plt.subplots(figsize=(20, 5))
-cmap = ['#cccccc', '#dfaaaa','#e67c73', '#d5d06f', '#abc978', '#81c281', '#57bb8a']
-
-tabela_dinamica = tabela_dinamica.sort_index(ascending=True)
-monthly_sessions = sns.heatmap(tabela_dinamica, 
-                    annot=True, 
-                    linewidths=0.5, 
-                    ax=ax, 
-                    cmap=cmap, 
-                    square=False)
-
-ax.set_xlabel("Semana em relação ao Deal",fontsize=15)
-ax.set_ylabel("Semanas de Abertura",fontsize=15)
-st.markdown("## Completude - Devolução")
-st.pyplot(plt.show())
-
+        #estrutura_coHort[str(f'{y} : {x}')] = ''
+        
+#print(estrutura_coHort)
