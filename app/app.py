cffc591014cc731940e6be4afedde5b88db17905
Miqueias97 <miqueiasnascimentonp@hotmail.com>
2024-06-13 12:35:05 -0300

    corrige dispositivo

--- app/app.py
+++ app/app.py
@@ -189,49 +189,50 @@ class Estruturacao_dos_dados():
             if cont > 0:
                 data_abertura_do_deal = i[8]
                 classe_do_pedido = i[9]
-                closed_date = i[5]
-                try:
-                    qtd_prevista = int(i[6])
-                except:
-                    qtd_prevista = None
-                try:
-                    qtd_devolvida = int(i[7])
-                except:
-                    qtd_devolvida = None
-                status_devolucao = i[3]
+                if classe_do_pedido in classes_para_filtro:
+                    closed_date = i[5]
+                    try:
+                        qtd_prevista = int(i[6])
+                    except:
+                        qtd_prevista = None
+                    try:
+                        qtd_devolvida = int(i[7])
+                    except:
+                        qtd_devolvida = None
+                    status_devolucao = i[3]
 
-                try:
-                    semana_de_abertura = datetime.strptime(str(data_abertura_do_deal).split('T')[0], '%Y-%m-%d')
-                    semana_de_abertura = int(semana_de_abertura.strftime("%U")) + 1
-                except:
-                    semana_de_abertura = None
+                    try:
+                        semana_de_abertura = datetime.strptime(str(data_abertura_do_deal).split('T')[0], '%Y-%m-%d')
+                        semana_de_abertura = int(semana_de_abertura.strftime("%U")) + 1
+                    except:
+                        semana_de_abertura = None
 
-                try:
-                    semana_de_conclusao = datetime.strptime(str(closed_date).split('T')[0], '%Y-%m-%d')
-                    semana_de_conclusao = int(semana_de_conclusao.strftime("%U")) + 1
-                    semanas_em_aberto_deal = semana_de_conclusao - semana_de_abertura
-                except:
-                    semana_de_conclusao = None
-                
-                if (qtd_devolvida != None or qtd_prevista != None) and classe_do_pedido in classes_para_filtro:
-                    if status_devolucao in status_concluidos:
-                        status = 'Concluido'
-                    else:
-                        status = 'Pendente'
+                    try:
+                        semana_de_conclusao = datetime.strptime(str(closed_date).split('T')[0], '%Y-%m-%d')
+                        semana_de_conclusao = int(semana_de_conclusao.strftime("%U")) + 1
+                        semanas_em_aberto_deal = semana_de_conclusao - semana_de_abertura
+                    except:
+                        semana_de_conclusao = None
                     
+                    if (qtd_devolvida != None or qtd_prevista != None):
+                        if status_devolucao in status_concluidos:
+                            status = 'Concluido'
+                        else:
+                            status = 'Pendente'
+                        
 
-                    deals[cont] = {
-                        'data_abertura_do_deal' : data_abertura_do_deal,
-                        'classe_do_pedido' : classe_do_pedido,
-                        'closed_date' : closed_date,
-                        'qtd_prevista' : qtd_prevista,
-                        'qtd_devolvida' : qtd_devolvida,
-                        'status_devolucao' : status_devolucao,
-                        'semana_de_abertura' : semana_de_abertura,
-                        'semana_de_conclusao' : semana_de_conclusao,
-                        'semanas_em_aberto_deal' : semanas_em_aberto_deal,
-                        'status' : status
-                    }
+                        deals[cont] = {
+                            'data_abertura_do_deal' : data_abertura_do_deal,
+                            'classe_do_pedido' : classe_do_pedido,
+                            'closed_date' : closed_date,
+                            'qtd_prevista' : qtd_prevista,
+                            'qtd_devolvida' : qtd_devolvida,
+                            'status_devolucao' : status,
+                            'semana_de_abertura' : semana_de_abertura,
+                            'semana_de_conclusao' : semana_de_conclusao,
+                            'semanas_em_aberto_deal' : semanas_em_aberto_deal,
+                            'status' : status
+                        }
         
         df_deals = []
         cols_df = ['deal_id', 'data_abertura_do_deal', 'classe_do_pedido', 'closed_date', 'qtd_prevista', 'qtd_devolvida', 'status_devolucao', \
@@ -243,9 +244,43 @@ class Estruturacao_dos_dados():
                 ])
         
         df_deals = pd.DataFrame.from_records(df_deals, columns=cols_df)
+        df_deals.to_csv('test.csv')
         return df_deals
 
+    def estruturacao_coHortsDisp(dataframe):
+        tamanho_dos_eixos =dataframe['semana_de_abertura'].max() if dataframe['semana_de_abertura'].max() > dataframe['semana_de_conclusao'].max() else dataframe['semana_de_conclusao'].max()
+        
+        # constroi eixo Y
+        dados_coHort = []
+        for cont, y in enumerate( range( 1, int(tamanho_dos_eixos) + 1 ) ):
+            # inicializa variaveis pré-definidas das semanas
+            acumulado_deal_semana, acumulado_dispositivos_semana = 0, 0
 
+            # Controi eixo X usando o contador que subtrai 1 a cada linha adicionada ao eixo Y
+            for x in range( 0, (int(tamanho_dos_eixos) + 1 ) - cont ): 
+                filtro = (dataframe['semana_de_abertura'] == y) & ( dataframe['semanas_em_aberto_deal'] == x ) & (dataframe['status_devolucao'] == 'Concluido')
+                df_semana = dataframe[filtro]
+                if len(df_semana) == 0:
+                    deals_concluidos, dispositivos_concluidos = 0, 0
+
+                else:
+                    deals_concluidos = 0
+                    dispositivos_concluidos = df_semana['qtd_devolvida'].sum()
+                    filtro = (dataframe['semana_de_abertura'] == y)
+                    df_semana = dataframe[filtro]
+                    acumulado_deal_semana = 0
+                    acumulado_dispositivos_semana = 0
+                    
+                dados_coHort.append([ y, x, deals_concluidos, dispositivos_concluidos, round(acumulado_deal_semana, 1), round(acumulado_dispositivos_semana, 1) ])
+                    
+
+        cols_df = ['semana_de_abertura', 'semana_em_relacao_ao_deal', 'qtd_deals_concluidos', 'qtd_disp_concluidos', 'per_acum_deal_semana', 'per_acum_disp_semana']
+        dados_coHort = pd.DataFrame(dados_coHort, columns=cols_df)
+        
+        return {
+        'coHort_deal' : dados_coHort.pivot_table(values='qtd_disp_concluidos', index='semana_de_abertura', columns='semana_em_relacao_ao_deal', aggfunc='sum'),
+        'coHort_deal_acum' : dados_coHort.pivot_table(values='per_acum_deal_semana', index='semana_de_abertura', columns='semana_em_relacao_ao_deal', aggfunc='sum')
+        }
 
 class Definicao_das_Views():
     # Defini configurações do streamlit
@@ -352,11 +387,23 @@ class Definicao_das_Views():
         if filtrar_classe != "Todas as Classes":
             df = dataframe[(dataframe['classe_do_pedido'] == filtrar_classe)]
             st.html(f'<h1 style="color : #666666">Completude - {pipeline} - {filtrar_classe}</h1>')
-            return df
+            return {
+                'df' : df,
+                'filtro' : str(filtrar_classe)
+            }
         else:
             st.html(f'<h1 style="color : #666666">Completude - {pipeline}</h1>')
-            return dataframe
+            return {
+                'df' : dataframe,
+                'filtro' : str(filtrar_classe)
+            }
         
+    def filtra_por_classe_disp(dataframe, filtrar_classe):
+        if filtrar_classe != "Todas as Classes":
+            df = dataframe[(dataframe['classe_do_pedido'] == filtrar_classe)]
+            return df
+        else:
+            return dataframe
 
 class Executa_app(Estruturacao_dos_dados, Definicao_das_Views):
     def __init__(self) -> None:
@@ -374,6 +421,10 @@ class Executa_app(Estruturacao_dos_dados, Definicao_das_Views):
                 df = Estruturacao_dos_dados.obtencao_dos_dados(classes, status_concluidos, pipeline)
                 df_disp = Estruturacao_dos_dados.estrutura_base_dispositivos(df['base'], classes, status_concluidos)
                 df = Definicao_das_Views.filtra_por_classe(df['df_deals'], pipeline)
+                df_disp = Definicao_das_Views.filtra_por_classe_disp(df_disp, df['filtro'])
+                df = df['df']
+                
+                
                 
 
                 # Descrição dos coHorts gerados:
@@ -386,7 +437,7 @@ class Executa_app(Estruturacao_dos_dados, Definicao_das_Views):
                     st.dataframe(df)
                 Estruturacao_dos_dados.tabela_resumida(df)
                 response = Estruturacao_dos_dados.estruturacao_coHorts(df)
-                responseDisp = Estruturacao_dos_dados.estruturacao_coHorts(df_disp)
+                responseDisp = Estruturacao_dos_dados.estruturacao_coHortsDisp(df_disp)
                 propriedades = Definicao_das_Views.propriedades_de_exibicao_coHort()
                 Definicao_das_Views.constroi_coHort(response['coHort_deal'], propriedades[1], propriedades[0], 'Fechamento por Deal Id', percentil=False)
                 Definicao_das_Views.constroi_coHort(response['coHort_deal_acum'], propriedades[1], propriedades[0], '% de Fechamento por Deal Id', percentil=True)
